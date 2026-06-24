1f91bd3e37debdba3ff79e9917073d7b2ffeb7b0
abdulloh <abdullohmirzayev7571@gmail.com>
2026-06-24 22:50:01 +0500

    added shadcn

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom'
 
 const DashboardLayout = () => {
-  return (
-    <div>
-      <h2>Qarz Manager</h2>
+	return (
+		<div>
+			<h2>Qarz Manager</h2>
 
-      <Outlet />
-    </div>
-  );
-};
+			<Outlet />
+		</div>
+	)
+}
 
-export default DashboardLayout;
+export default DashboardLayout
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
+import "./App.css";
+
 import { router } from "./ruotes";
 
 import AuthProvider from "./components/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>,
+	<AuthProvider>
+		<RouterProvider router={router} />
+	</AuthProvider>,
 );
